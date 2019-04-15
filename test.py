019624da695e4e96eca042c8584c4d86ee685ec7
Sofia AF <sofia@afsofia.dev>
2019-04-15 21:44:20 +0200

    update test harness

--- test.py
+++ test.py
@@ -17,6 +17,37 @@ import subprocess
 interp = 'bootstrap/mirth.py'
 subprocess.run([interp, '--doctest'])
 
-for path in glob.glob('bootstrap/tests/pass/*.mth'):
-    subprocess.run([interp, path])
+for path in glob.glob('bootstrap/tests/pass/*'):
+    with subprocess.Popen([interp, path],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE) as proc:
+
+        (outs, errs) = proc.communicate()
+        if proc.returncode > 0:
+            print ("TEST FAILED [", path, "]")
+            if outs: print ("  STDOUT:", outs.decode('utf8'))
+            if errs: print ("  STDERR:", errs.decode('utf8'))
+
+for path in glob.glob('bootstrap/tests/fail_types/*'):
+    with subprocess.Popen([interp, path],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE) as proc:
+
+        (outs, errs) = proc.communicate()
+        if 'TypeError' not in errs.decode('utf8'):
+            print ("TEST FAILED [", path, "]")
+            if outs: print ("  STDOUT:", outs.decode('utf8'))
+            if errs: print ("  STDERR:", errs.decode('utf8'))
+
+for path in glob.glob('bootstrap/tests/fail_tests/*'):
+    with subprocess.Popen([interp, path],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE) as proc:
+
+        (outs, errs) = proc.communicate()
+        if 'Assertion failed' not in errs.decode('utf8'):
+            print ("\nTEST FAILED [", path, "]")
+            if outs: print ("\nSTDOUT:\n\n" + outs.decode('utf8') + '\n')
+            if errs: print ("\nSTDERR:\n\n" + errs.decode('utf8') + '\n')
+            print ()
 
