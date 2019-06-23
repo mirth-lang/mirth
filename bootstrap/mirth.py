917a629849afe35b204d3579b68e4627b046cbea
Sofia AF <sofia@afsofia.dev>
2019-06-23 17:13:57 +0100

    Update REPL credits

--- bootstrap/mirth.py
+++ bootstrap/mirth.py
@@ -105,7 +105,7 @@ def repl(flags):
     print('Mirth Bootstrap Interpreter v0.0.0: CAVEAT USOR.')
     print()
     print('Thank you to our patrons on https://patreon.com/mirth_lang')
-    print('And a special thanks to Benjohn.')
+    print('And a special thanks to Benjohn, Joseph Victor, and spacekitteh')
     print()
 
     m = load_prelude() if not flags['no-prelude'] else module()
