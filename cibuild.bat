25ba54e791be72cab85788cde7cb5db866ac1b1e
Sofia Faro <sofia@afsofia.dev>
2020-04-13 19:04:11 +0100

    fix cibuild.bat

--- cibuild.bat
+++ cibuild.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1 || exit /b 1
 
-".\build.bat"
+"build.bat" || exit /b 2
