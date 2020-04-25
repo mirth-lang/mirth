aae40f44bfdf0e8351b188a219016d54a1f0144f
Sofia Faro <sofia@afsofia.dev>
2020-04-25 09:39:59 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,22 +1,19 @@
 @echo off
 
-cl /WX /MD /TC mirth0.c /link /WX || goto :error
-if exist mirth0.obj del mirth0.obj
-mirth0.exe || goto :error
-if exist mirth1.c del mirth1.c
-rename mirth.c mirth1.c || goto :error
+cl /WX /MD /Fe bin\ /TC bin\mirth0.c /link /WX || goto :error
+bin\mirth0.exe || goto :error
+if exist bin\mirth1.c del bin\mirth1.c
+rename bin\mirth.c bin\mirth1.c || goto :error
 
-cl /WX /MD /TC mirth1.c /link /WX || goto :error
-if exist mirth1.obj del mirth1.obj
-mirth1.exe || goto :error
-if exist mirth2.c del mirth2.c
-rename mirth.c mirth2.c || goto :error
+cl /WX /MD /Fe bin\ /TC bin\mirth1.c /link /WX || goto :error
+bin\mirth1.exe || goto :error
+if exist bin\mirth2.c del bin\mirth2.c
+rename bin\mirth.c bin\mirth2.c || goto :error
 
-cl /WX /W3 /MD /TC mirth2.c /link /WX || goto :error
-if exist mirth2.obj del mirth2.obj
-mirth2.exe || goto :error
-if exist mirth3.c del mirth3.c
-rename mirth.c mirth3.c || goto :error
+cl /WX /W3 /MD /Fe bin\ /TC mirth2.c /link /WX || goto :error
+bin\mirth2.exe || goto :error
+if exist bin\mirth3.c del bin\mirth3.c
+rename bin\mirth.c bin\mirth3.c || goto :error
 
 goto :EOF
 
