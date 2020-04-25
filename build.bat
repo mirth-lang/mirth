8cc13210e810137793aeb62c5f5af26fa0eb7c30
Sofia Faro <sofia@afsofia.dev>
2020-04-25 10:04:28 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,19 +1,19 @@
 @echo off
 
 cl /WX /MD /TC bin\mirth0.c /link /WX  || goto :error
-bin\mirth0.exe || goto :error
-if exist bin\mirth1.c del bin\mirth1.c
-rename bin\mirth.c bin\mirth1.c || goto :error
+mirth0.exe || goto :error
+if exist "bin\mirth1.c" del "bin\mirth1.c"
+rename "bin\mirth.c" "bin\mirth1.c" || goto :error
 
 cl /WX /MD /TC bin\mirth1.c /link /WX  || goto :error
-bin\mirth1.exe || goto :error
-if exist bin\mirth2.c del bin\mirth2.c
-rename bin\mirth.c bin\mirth2.c || goto :error
+mirth1.exe || goto :error
+if exist "bin\mirth2.c" del "bin\mirth2.c"
+rename "bin\mirth.c" "bin\mirth2.c" || goto :error
 
 cl /WX /W3 /MD /TC bin\mirth2.c /link /WX  || goto :error
-bin\mirth2.exe || goto :error
-if exist bin\mirth3.c del bin\mirth3.c
-rename bin\mirth.c bin\mirth3.c || goto :error
+mirth2.exe || goto :error
+if exist "bin\mirth3.c" del "bin\mirth3.c"
+rename "bin\mirth.c" "bin\mirth3.c" || goto :error
 
 goto :EOF
 
