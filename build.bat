01e92d0854ed87cb6c631881b9d960b8dd1538e4
typeswitch-dev <typeswitch@protonmail.com>
2024-04-01 09:43:06 +0100

    fixing build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
 
 cl /WX /MD /TC bin\mirth0.c /link /WX  || goto :error
-mirth0.exe mirth.mth -o mirth1.c || goto :error
+mirth0.exe mirth\main.mth -o mirth1.c || goto :error
 
 cl /WX /MD /TC bin\mirth1.c /link /WX  || goto :error
-mirth1.exe mirth.mth -o mirth2.c || goto :error
+mirth1.exe mirth\main.mth -o mirth2.c || goto :error
 
 cl /WX /W3 /MD /TC bin\mirth2.c /link /WX  || goto :error
-mirth2.exe mirth.mth -o mirth3.c || goto :error
+mirth2.exe mirth\main.mth -o mirth3.c || goto :error
 
 goto :EOF
 
