f8e5c9f248dd0160493d83c1a3f0e7a97ceb33dc
typeswitch-dev <typeswitch@protonmail.com>
2024-05-12 12:48:56 +0100

    update cibuild

--- tools/cibuild.bat
+++ tools/cibuild.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1 && "build.bat" || exit /b 2
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1 && "tools\build.bat" || exit /b 2
