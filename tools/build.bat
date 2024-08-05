491ac0e3d7cce259f8c0330e7c882d57ef2c9918
casuallyblue <sierra@casuallyblue.dev>
2024-08-05 21:15:11 +0000

    move link argument

--- tools/build.bat
+++ tools/build.bat
@@ -1,12 +1,12 @@
 @echo off
 
-cl /WX /MD /TC bin\mirth0.c /Fobin\wmirth0.obj  /Febin\wmirth0.exe legacy_stdio_definitions.lib /link /WX || goto :error
+cl /WX /MD /TC bin\mirth0.c /Fobin\wmirth0.obj  /Febin\wmirth0.exe /link legacy_stdio_definitions.lib /WX || goto :error
 bin\wmirth0.exe src\main.mth -o bin\wmirth1.c || goto :error
 
-cl /WX /MD /TC bin\wmirth1.c /Fobin\wmirth1.obj /Febin\wmirth1.exe legacy_stdio_definitions.lib /link /WX || goto :error
+cl /WX /MD /TC bin\wmirth1.c /Fobin\wmirth1.obj /Febin\wmirth1.exe /link legacy_stdio_definitions.lib /WX || goto :error
 bin\wmirth1.exe src\main.mth -o bin\wmirth2.c || goto :error
 
-cl /WX /W3 /MD /TC bin\wmirth2.c /Fobin\wmirth2.obj /Febin\wmirth2.exe legacy_stdio_definitions.lib /link /WX || goto :error
+cl /WX /W3 /MD /TC bin\wmirth2.c /Fobin\wmirth2.obj /Febin\wmirth2.exe /link legacy_stdio_definitions.lib /WX || goto :error
 bin\wmirth2.exe src\main.mth -o bin\wmirth3.c || goto :error
 
 goto :EOF
