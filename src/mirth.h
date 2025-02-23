82fd7d7505bace49d53e50e077f369cc930c4948
typeswitch-dev <typeswitch@protonmail.com>
2025-02-23 13:45:32 +0000

    Double dispatch on arithmetic & relational operations for number types. (#377)
    This PR uses the existing single-dispatch mechanism to implement
    arithmetic & relational operators with double dispatch. As a result we
    don't typically need to convert between number types. For instance, if
    you add a `Nat` and an `Int` you get an `Int` as a result. The goal is
    to always return the correct value or result according to mathematics,
    so there is no danger of overflow or unexpected results. The output type
    is chosen so that it is large enough to represent the outcome, e.g.
    adding two `I64` will result in an `Int`, not an `I64`. Dividing two
    integers results in a rational number (`Rat`). Floats are inexact, so
    when you have an operation using floats (`F32` or `F64`) you will always
    get a float as a result.
    
    How double dispatch is implemented via single dispatch: The idea is to
    turn a method on the top argument into a distinct method on the second
    argument based on the top argument's type. If you look in `std.number`
    you'll see how this was achieved. When calling `Int.+`, it pushes the
    top number onto the `int` stack label and calls the `int+` method on the
    second argument. When calling `Nat.+`, it pushes the top number onto the
    `nat` stack label and calls the `nat+` method on the second argument.
    Thus if you have a type implement `int+` and `nat+` it can adjust the
    return types based on the value.
    
    In addition, this PR also:
    
    - Moves the number methods from `std.prelude` to `std.number`. You don't
    need to import `std.number` directly.
    - Adds a number type `Zero` which represents only the number 0.
    - Adds a rational number type `Rat`.
    - Adds a complex number type `Complex(a,b)`, parametrized on the types
    for the real and imaginary components.
    - Merges `USize` and `ISize` types as `Size`.
    - Merges `UOffset` and `IOffset` types as `Offset`.
    - Removes `UIndex`.
    - Changes the types in std.posix to take advantage of resources a bit
    better, and allow for error handling.
    - Changes `==` to `=` (but `==` still works).

--- src/mirth.h
+++ src/mirth.h
@@ -1113,7 +1113,12 @@ static void big_pos_udivmod(BIG* a, INT b, INT *q, INT *r) {
     if (IS_BIG(b)) {
         BIG* bb = GET_BIG(b);
         bshift = bb->size-1;
-        bhead = bb->radix[bb->size-1];
+        bhead = bb->radix[bshift];
+        if (bhead == 0) {
+            ASSERT(bshift > 0);
+            bshift--;
+            bhead = bb->radix[bshift];
+        }
         for (size_t i = 0; i < bshift; i++) {
             if (bb->radix[i]) { bperfect=false; break; }
         }
@@ -1285,9 +1290,12 @@ static bool int_ne(INT a, INT b) { return int_cmp(a,b) != 0; }
 
 
 static STR* i64_to_str(int64_t);
+static STR* u64_to_str(uint64_t);
 static STR* int_to_str(INT a) {
     if (IS_I63(a) || (GET_BIG(a)->size == 2)) {
         return i64_to_str(int_to_i64(a));
+    } else if ((GET_BIG(a)->size == 3) && (GET_BIG(a)->radix[2] == 0)) {
+        return u64_to_str(int_to_u64(a));
     } else {
         STR *s = str_alloc(GET_BIG(a)->size * 10);
         if (SIGN_RADIX(GET_BIG(a))) {
