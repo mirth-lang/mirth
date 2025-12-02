c7ddcef127d1f9047858dfcc12227327be4418e8
typeswitch <typeswitch@protonmail.com>
2025-12-02 14:57:16 +0000

    Updated sublime plugin

---
+++ tools/install-sublime.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH="${BASH_SOURCE[0]:-$0}"
+echo "${SCRIPT_PATH}"
+
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+echo "$SCRIPT_DIR"
+
+LIN_PATH=~/.config/sublime-text-3
+OSX_PATH=~/Library/"Application Support"/"Sublime Text"
+
+if [ -d "$OSX_PATH" ] ; then
+	CONFIG_PATH="$OSX_PATH"
+elif [ -d "$LIN_PATH" ] ; then
+	CONFIG_PATH="$LIN_PATH"
+else
+	echo "Failed to find sublime config dir. Please install $SCRIPT_DIR/mirth-sublime manually."
+	exit 1
+fi
+
+echo "Found sublime config dir at '$CONFIG_PATH'"
+PACKAGE_PATH="$CONFIG_PATH/Packages/Mirth"
+
+echo "Copying '$SCRIPT_DIR/mirth-sublime' to '$PACKAGE_PATH'"
+
+rm -f "$PACKAGE_PATH/*"
+cp -r "$SCRIPT_DIR/mirth-sublime" "$PACKAGE_PATH"
